7be616f6920d04c0be065b5b291eeeca9d91950a
XiaoYuan151 <xiaoyuan151@outlook.com>
2026-02-10 14:43:48 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+async function handleRequest(request) {
+  const url = new URL(request.url);
+  const shortCode = url.pathname.split("/").filter(Boolean)[0];
+  const defaultUrl = await LINK_DB.get("default");
+  if (!shortCode) {
+    return Response.redirect(defaultUrl, 301);
+  }
+  const longUrl = await LINK_DB.get(shortCode);
+  if (longUrl) {
+    return Response.redirect(longUrl, 302);
+  } else {
+    return Response.redirect(defaultUrl, 301);
+  }
+}
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
